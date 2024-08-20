92f7eab66ffa0c13914d5cd28ebf0747fd3555bf
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-08-20 18:35:39 -0300

    feat: add header interface

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import logo from "@/assets/logo.svg";
+
+export const Header = () => {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logo} alt="" />
+
+        <NewTransactionButton>Nova Transação</NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  );
+};
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  background-color: ${(props) => props.theme.colors["gray-900"]};
+  padding: 2.5rem 0 7.625rem;
+`;
+
+export const HeaderContent = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+  height: 3.125rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const NewTransactionButton = styled.button`
+  background-color: ${(props) => props.theme.colors["green-500"]};
+  color: ${(props) => props.theme.colors.white};
+  font-weight: bold;
+  padding: 0.75rem 1.25rem;
+  border-radius: 6px;
+  border: 0;
+
+  &:not(:disabled):hover {
+    background-color: ${(props) => props.theme.colors["green-300"]};
+    transition: background-color 0.2s;
+  }
+`;
