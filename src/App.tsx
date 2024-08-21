1a0ebfeac30bc561a70c2e3ff5c8a1e7038c5767
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-08-21 14:24:04 -0300

    feat: add transactions page

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header";
-import { Summary } from "./components/Summary";
+import { Transactions } from "./pages/Transactions";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <Header />
-      <Summary />
-      Hello World!
+
+      <Transactions />
     </ThemeProvider>
   );
 }
---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,12 @@
+import { Summary } from "@/pages/Transactions/components/Summary";
+import { SearchForm } from "./components/SearchForm";
+
+export const Transactions = () => {
+  return (
+    <>
+      <Summary />
+
+      <SearchForm />
+    </>
+  );
+};
